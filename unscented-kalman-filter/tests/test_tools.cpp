a36f2ec4d5c7090b271225ffad5a4078b84e82b0
Moises Vargas M <moisesweb@gmail.com>
2018-05-07 15:04:41 -0500

    RMSE

--- unscented-kalman-filter/tests/test_tools.cpp
+++ unscented-kalman-filter/tests/test_tools.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using std::vector;
 
 TEST(Tools, GenerateSigmas){
     int n_x = 5;
@@ -219,4 +220,31 @@ TEST(Tools, UpdateState){
 	t.UpdateState(z, z_pred, x_sig, z_sig, s, &x, &p);
 	ASSERT_TRUE(x.isApprox(x_expected, 0.00001));
 	ASSERT_TRUE(p.isApprox(p_expected, 0.00001));
+}
+
+TEST(Tools, RMSE) {
+	vector<VectorXd> estimations;
+	vector<VectorXd> ground_truth;
+
+	VectorXd e(4);
+	e << 1, 1, 0.2, 0.1;
+	estimations.push_back(e);
+	e << 2, 2, 0.3, 0.2;
+	estimations.push_back(e);
+	e << 3, 3, 0.4, 0.3;
+	estimations.push_back(e);
+
+	VectorXd g(4);
+	g << 1.1, 1.1, 0.3, 0.2;
+	ground_truth.push_back(g);
+	g << 2.1, 2.1, 0.4, 0.3;
+	ground_truth.push_back(g);
+	g << 3.1, 3.1, 0.5, 0.4;
+	ground_truth.push_back(g);
+
+    Tools::Tools t;
+	VectorXd expected(4);
+	expected << 0.1,0.1,0.1,0.1;
+
+    ASSERT_TRUE( t.CalculateRMSE(estimations, ground_truth).isApprox(expected) );
 }
